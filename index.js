c380f1f005db1e7f59287df9f613d45d9e9e4a82
Justin McMillion <79409949+jmcmillion89@users.noreply.github.com>
2024-02-01 21:26:12 -0500

    create and style grid

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 const container = document.querySelector('.container')
-const num = 256
+const gridNumber = 64
+const gridSize = 650 / gridNumber + 'px'
 
-for (let i = 0; i < num; i++) {
-    container.innerHTML += '<div class="box"></div>';
-}
+// create columns for grid
+
+for (let i = 0; i < gridNumber; i++) {
+    const div = document.createElement('div')
+    div.className = 'column';
+    container.appendChild(div);
+    }
+
+// create cells for grid
+
+const columns = document.querySelectorAll('.column')
+
+columns.forEach(column => {
+    for (let i =0; i < gridNumber; i++) {
+        const div = document.createElement('div')
+        div.className = 'box';
+        div.style.width = gridSize;
+        column.appendChild(div);
+    }
+})
+
+// change color of cell when hovering
+
+let box = document.querySelectorAll('.box')
+
+box.forEach((e) => {
+    
+    e.addEventListener('mouseover', () => {
+        e.style.backgroundColor = 'black'
+    })
+})
