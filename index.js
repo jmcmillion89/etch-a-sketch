1a7cb5b11126a8b50df43ef5b7c727cba4ec052f
Justin McMillion <79409949+jmcmillion89@users.noreply.github.com>
2024-02-02 18:09:52 -0500

    create mouseover function

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 const container = document.querySelector('.container')
 const changeGridNumber = document.querySelector('#gridSize')
-
-
-let boxes = document.querySelectorAll('.box')
-boxes.forEach((box) => {
-    box.addEventListener('click', () => {
-        box.style.backgroundColor = 'black'
-    })
-})
+const columns = document.querySelectorAll('.column')
+const color = 'green'
 
 // button function change the size of grid
-changeGridNumber.addEventListener('click', function(){
+changeGridNumber.addEventListener('click', function (){
     let number = prompt('Choose Grid Size Between 1-100')
-    if (number >= 1 || number <= 100) {
-        gridNumber = number,
-        removeGrid()
-        createGrid(number)
-    }
-
-})
+    if (number >= 1 && number <= 100) {
+        (gridNumber = number,
+        removeGrid(),
+        createGrid(number),
+        changeColor())
+    } else {
+        alert('Grid Size Must Be Between 1-100')
+    }});
 
 // removes the grid
 function removeGrid() {
@@ -26,77 +21,34 @@ function removeGrid() {
     columns.forEach((column) => {
         while (column.firstChild) {
             column.removeChild(column.firstChild);
-
-        }
-        column.parentNode.removeChild(column);
-        })}
+            } column.parentNode.removeChild(column);
+                })};
 
 //function to create the grid
 function createGrid(number) {
-
-
-for (let i = 0; i < number; i++) {
-    const div = document.createElement('div')
-    div.className = 'column';
-    container.appendChild(div);
-    }
-
-const columns = document.querySelectorAll('.column')
-
-columns.forEach(column => {
-    for (let i =0; i < number; i++) {
-        const gridSize = 650 / number + 'px'
+    for (let i = 0; i < number; i++) {
         const div = document.createElement('div')
-        div.className = 'box';
-        div.style.width = gridSize;
-        column.appendChild(div);
+        div.className = 'column';
+        container.appendChild(div);
     }
-})}
 
-// change color of cell when hovering
+        const columns = document.querySelectorAll('.column')
 
+        columns.forEach(column => {
+            for (let i =0; i < number; i++) {
+                const gridSize = 650 / number + 'px'
+                const div = document.createElement('div')
+                div.className = 'box';
+                div.style.width = gridSize;
+                column.appendChild(div);
+    }})};
 
+// change color of boxes on mouseover
 
-
-// function createGrid(gridNumber) {
-    
-//     const gridSize = 650 / gridNumber + 'px'
-//     const changeGridNumber = document.querySelector('#gridSize')
-
-//     for (let i = 0; i < gridNumber; i++) {
-//         const div = document.createElement('div')
-//         div.className = 'column';
-//         container.appendChild(div);
-        
-        
-//         const columns = document.querySelectorAll('.column')
-        
-//         for (let i = 0; i < gridNumber; i++) {
-//         const div = document.createElement('div')
-//         div.className = 'box';
-//         div.style.width = gridSize;
-//         column.appendChild(div);
-//             }}
-// }
-
-// create columns for grid
-
-// for (let i = 0; i < gridNumber; i++) {
-//     const div = document.createElement('div')
-//     div.className = 'column';
-//     container.appendChild(div);
-//     }
-
-// // create cells for grid and set size per cell
-
-// const columns = document.querySelectorAll('.column')
-
-// columns.forEach(column => {
-//     for (let i =0; i < gridNumber; i++) {
-//         const div = document.createElement('div')
-//         div.className = 'box';
-//         div.style.width = gridSize;
-//         column.appendChild(div);
-//     }
-// })
-
+function changeColor() {
+    const boxes = document.querySelectorAll('.box')
+        boxes.forEach((box) => {
+            box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = color
+    });
+})};
